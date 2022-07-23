ab69e477dac3d2ec0981898779e45752b07f01b5
YusufDeniz1627 <myusufdeniz8@gmail.com>
2022-07-23 22:40:41 +0300

    w10h1

---
+++ ReCap/Business/Constants/Messages.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //Car
+        public static string CarAdded = "Araç Eklendi";
+        public static string CarNameInvalid = "Araç İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string CarListed="Araçlar Listelendi";
+        public static string CarColorIdListed="Araçlar Renk Sırasına Göre Listelendi";
+        public static string CarDeleted="Araç Silinmiştir";
+        public static string CarUpdated="Araç Güncellenmiştir";
+        //Brand
+        public static string BrandNameInvalid="Marka İsmi Geçersiz";
+        public static string BrandListed="Markalar Listelendi";
+        //Color
+        public static string ColorNameInvalid="Renk İsmi Geçersiz";
+        internal static string ColorListed="Renkler Listelendi";
+    }
+}
---
+++ ReCap/ConsoleUI/Program.cs
@@ -0,0 +1,69 @@
+﻿using Business.Concrete;
+using System;
+using DataAccess.Concrete.InMemory;
+using Entity.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CarTest();
+            //BrandTest();
+            //ColorTest();
+            //CarManager carManager = new CarManager(new EfCarDal());
+            //foreach (var item in carManager.GetCarDetail())
+            //{
+            //    Console.WriteLine("isim :"+item.CarName+"\nmarka:"+item.BrandName+"\nrenk :"+item.ColorName+"\ngünlük fiyat :"+item.DailyPrice);
+            //}
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            //Color color = new Color { Id = 2, Name = "Beyaz" };
+            //colorManager.Add(color);
+            var result = colorManager.GetAll();
+            foreach (var item in result.Data)
+            {
+               Console.WriteLine(item.Name);
+            }
+            Console.WriteLine(colorManager.GetById(2).Data.Name);
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            //Brand brand = new Brand { Id = 2, Name = "BMW" };
+            //brandManager.Delete(brand);
+            var result = brandManager.GetAll();
+            foreach (var item in result.Data)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine(brandManager.GetbyId(1).Data.Name);
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            //Car newCar = new Car {Id=1,BrandId=1,ColorId=1,ModelYear=2023,DailyPrice=600,CarName="Togg",Detail="Yerli SUV TOGG" };
+            //carManager.Add(newCar);
+            var result = carManager.GetAll();
+            if (result.Success == true)
+            {
+                foreach (var car in result.Data)
+                {
+                    Console.WriteLine(car.CarName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+    }
+}
